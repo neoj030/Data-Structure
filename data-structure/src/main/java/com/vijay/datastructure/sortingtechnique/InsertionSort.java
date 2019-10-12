40c2aa653d8e8614c12b8c0de3d6c35b8cafcda8
Neo <neoj030@gmail.com>
2019-10-12 11:26:29 +0530

    Insertion sort implementation has been added

--- data-structure/src/main/java/com/vijay/datastructure/sortingtechnique/InsertionSort.java
+++ data-structure/src/main/java/com/vijay/datastructure/sortingtechnique/InsertionSort.java
@@ -4,7 +4,30 @@ public class InsertionSort {
 	
 	public static int[] insertionSort(int[] arr) {
 		
+		int i,j;
+		for(i=1; i<arr.length; i++) {
+			j=i-1;
+			while(j>=0&&arr[j]>arr[i]) {
+				arr[j+1]=arr[j+1]^arr[j];
+				arr[j]=arr[j+1]^arr[j];
+				arr[j+1]=arr[j+1]^arr[j];
+				
+				j--;
+			}			
+		}		
 		return arr;
+	}
+	
+	public static void main(String[] args) {
+int[] arr = {7,8,5,2,4,6,3};
+		
+		int[] result =BubbleSort.bubbleSort(arr);
+		
+		System.out.print("[");
+		for(int i: result) {
+			System.out.print(i+" ");
+		}
+		System.out.print("]");
 	}
 
 }
---
+++ data-structure/src/test/java/com/vijay/datastructure/sortingtechnique/InsertionSortTest.java
@@ -0,0 +1,18 @@
+package com.vijay.datastructure.sortingtechnique;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class InsertionSortTest {
+
+	@Test
+	@DisplayName("Test Insertion sort")
+	void test() {
+		int[] arr = {7,8,5,2,4,6,3};
+		int[] expected = {2,3,4,5,6,7,8};
+		assertArrayEquals(expected, BubbleSort.bubbleSort(arr));
+	}
+
+}
